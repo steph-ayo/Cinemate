171b3adefd40252d7d0e7668680980fd6a8399b2
steph-ayo <popoolastephen19@gmail.com>
2023-11-18 23:48:06 +0100

    app update with useRefs

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
 
 const average = (arr) =>
@@ -174,11 +174,25 @@ function Logo() {
 
 // --------------SEARCH COMPONENT (START)---------------
 function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
   useEffect(function () {
-    const el = document.querySelector(".search");
-    console.log(el);
-    el.focus();
+    function callback(e) {
+      if (document.activeElement === inputEl.current) return;
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    }
+    document.addEventListener("keydown", callback);
+    return () => document.addEventListener("keydown", callback);
   }, []);
+
+  // useEffect(function () {
+  //   const el = document.querySelector(".search");
+  //   console.log(el);
+  //   el.focus();
+  // }, []);
 
   return (
     <input
@@ -187,6 +201,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
