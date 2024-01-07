f409e386df0421bc34b1bf478ebbb5addf8e034d
steph-ayo <popoolastephen19@gmail.com>
2024-01-07 22:19:28 +0100

    NumResults update

--- src/components/NumResults.js
+++ src/components/NumResults.js
@@ -1,6 +1,8 @@
+import "animate.css";
+
 export default function NumResults({ movies }) {
   return (
-    <p className="num-results">
+    <p className="num-results animate__animated animate__bounce">
       Found <strong>{movies.length}</strong> results
     </p>
   );
